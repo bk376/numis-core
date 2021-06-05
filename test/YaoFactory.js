159cb85f24bca6d32628130211b6ff570925da40
bk376 <benardkipcoech@gmail.com>
2021-06-05 16:03:05 -0400

    initialize testing template

---
+++ test/YaoFactory.js
@@ -0,0 +1,54 @@
+// test/Airdrop.js
+// Load dependencies
+const { expect } = require('chai');
+const { BigNumber } = require('ethers');
+const { ethers } = require('hardhat');
+const Web3 = require('web3');
+
+const OWNER_ADDRESS = ethers.utils.getAddress(process.env.OWNER_ADDRESS);
+
+const DECIMALS = 10;
+
+const AMT = 150
+
+
+
+// Start test block
+describe('YaoFactory', async () => {
+    const [deployer] = await ethers.getSigners()
+    before(async function () {
+        this.Factory = await ethers.getContractFactory("YaoFactory");
+        this.MockContract = await ethers.getContractFactory("contracts/MockContract.sol:MockContract");
+    });
+
+    beforeEach(async function () {
+        this.Factory = await this.Factory.deploy(deployer.address)
+        await this.factory.deployed()
+        this.mock = await this.MockContract.deploy()
+        await this.mock.deployed()
+    });
+
+    // Test cases
+
+    //////////////////////////////
+    //       Constructor 
+    //////////////////////////////
+    describe("Constructor", function () {
+        it('decimals default', async function () {
+            expect((await this.factory.decimals())).to.equal(DECIMALS);
+        });
+
+        it('mock test', async function () {
+            // If another contract calls balanceOf on the mock contract, return AMT
+            const balanceOf = Web3.utils.sha3('balanceOf(address)').slice(0,10);
+            await this.mock.givenMethodReturnUint(balanceOf, AMT);
+        });
+    });
+
+    //////////////////////////////
+    //  setRemainderDestination 
+    //////////////////////////////
+    describe("otherMethod", function () {
+        console.log('Howdy fellow man')
+    });
+});
